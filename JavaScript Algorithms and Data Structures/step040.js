3f6cc4b5c567b1d2000a3a6489823e44ddfb81c8
Preetham_dheeraj <32894920+preethamdheeraj@users.noreply.github.com>
2023-03-15 07:24:40 +0530

    Add files via upload

---
+++ JavaScript Algorithms and Data Structures/step040.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
---
+++ JavaScript Algorithms and Data Structures/step044.js
@@ -0,0 +1,8 @@
+// Setup
+let myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+
+// Only change code below this line
+myArray[2] = ["dog", 3];
