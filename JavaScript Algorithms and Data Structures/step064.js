9e1379c0272e30c95b6ea3e819f005d8e3749841
Preetham_dheeraj <32894920+preethamdheeraj@users.noreply.github.com>
2023-03-15 16:18:59 +0530

    Add files via upload

--- JavaScript Algorithms and Data Structures/step064.js
+++ JavaScript Algorithms and Data Structures/step064.js
@@ -1,10 +1,10 @@
 // Setup
-function testNotEqual(val) {
-  if (val != 99) {
+function testStrictNotEqual(val) {
+  if (val !== 17) {
     // Change this line
     return "Not Equal";
   }
   return "Equal";
 }
 
-testNotEqual(10);
+testStrictNotEqual(10);
