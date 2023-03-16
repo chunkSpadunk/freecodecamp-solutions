31b35fc44b0708d993cbe5ea5caaeb87fc6a0e73
Preetham_dheeraj <32894920+preethamdheeraj@users.noreply.github.com>
2023-03-16 11:19:30 +0530

    Add files via upload

---
+++ JavaScript Algorithms and Data Structures/step082.js
@@ -0,0 +1,13 @@
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+    if (a < 0 || b < 0) {
+        return undefined; //  remember undefined is a keyword not a string
+    }
+
+    // Only change code above this line
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+console.log(abTest(-2, 2));
---
+++ JavaScript Algorithms and Data Structures/step084.js
@@ -0,0 +1,5 @@
+const myDog = {
+    // Only change code below this line
+    // we are going to write the properties of objects now
+    // Only change code above this line
+};
