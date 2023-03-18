a325330eafb64b19171792d27ab1aada7af21e3d
Preetham_dheeraj <32894920+preethamdheeraj@users.noreply.github.com>
2023-03-18 07:50:11 +0530

    Add files via upload

---
+++ JavaScript Algorithms and Data Structures/step093.js
@@ -0,0 +1,30 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  
+  // we just need to add another record here
+  {
+    
+    "artist": "this is the artist name",
+    "title": "this is the title",
+    "release_year": 2001,
+    "formats": [
+      "first format",
+      "second format",
+      "third format"
+    ],
+    "just in time": false
+  }
+
+];
+
+// another element had to be added this is an example of a complex data structure in javascript
---
+++ JavaScript Algorithms and Data Structures/step095.js
@@ -0,0 +1,22 @@
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+
+// the array bracket notation can be chained to use the index calls for the respective objects
