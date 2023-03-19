3e7687e43ce365fb4b5de1b99acf083c77486db3
Preetham_dheeraj <32894920+preethamdheeraj@users.noreply.github.com>
2023-03-19 07:46:15 +0530

    Add files via upload

---
+++ JavaScript Algorithms and Data Structures/step097.js
@@ -0,0 +1,11 @@
+// Setup
+const myArray = [];
+let i = 0;
+
+// Only change code below this line
+while (i <= 5) {
+    myArray.push(i);
+    i++;
+}
+myArray.reverse();
+console.log(myArray);
---
+++ JavaScript Algorithms and Data Structures/step100.js
@@ -0,0 +1,11 @@
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 0; i <= 9; i++) {
+    if (i % 2 != 0) {
+        myArray.push(i);
+    }
+}
+
+console.log(myArray);
