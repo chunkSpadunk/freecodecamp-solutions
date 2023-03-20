1da34272b26aa5c152f0a3c4311007d235a70901
Preetham_dheeraj <32894920+preethamdheeraj@users.noreply.github.com>
2023-03-20 14:45:21 +0530

    Add files via upload

---
+++ JavaScript Algorithms and Data Structures/step101.js
@@ -0,0 +1,11 @@
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 10; i >= 0; i--) {
+    if (i % 2 != 0) {
+        myArray.push(i);
+    }
+}
+
+console.log(myArray);
