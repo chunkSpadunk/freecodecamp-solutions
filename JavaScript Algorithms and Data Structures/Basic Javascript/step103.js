12b0818a1732d880db3c036267a7445925c0bf47
Preetham_dheeraj <32894920+preethamdheeraj@users.noreply.github.com>
2023-03-21 12:03:36 +0530

    Add files via upload

---
+++ JavaScript Algorithms and Data Structures/Basic Javascript/step103.js
@@ -0,0 +1,12 @@
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line
+// you need to change the code below this line to use the do while loop
+do {
+    myArray.push(i);
+    i++;
+} while (i <= 10);
+console.log(i);
+console.log(myArray);
---
+++ JavaScript Algorithms and Data Structures/Basic Javascript/step107.js
@@ -0,0 +1,9 @@
+function randomWholeNum() {
+    // Only change code below this line
+    // to make the random number between a two numbers we have to do it the following the way
+    var result = 0;
+    result = Math.floor(Math.random() * 10);
+    return result;
+}
+
+randomWholeNum();
