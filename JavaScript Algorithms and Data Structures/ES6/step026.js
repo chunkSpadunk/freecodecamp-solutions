9a57a09c67edf4766f67eda724a304497f18303d
Preetham_dheeraj <32894920+preethamdheeraj@users.noreply.github.com>
2023-03-22 09:00:10 +0530

    Add files via upload

--- JavaScript Algorithms and Data Structures/ES6/step026.js
+++ JavaScript Algorithms and Data Structures/ES6/step026.js
@@ -1,3 +1,3 @@
 // A promise in JavaScript is exactly what it sounds like - you use it to make a promise to do something, usually asynchronously
 
-const makeServerRequest = new Promise ((resolve, reject) => {});
+const makeServerRequest = new Promise((resolve, reject) => {});
--- JavaScript Algorithms and Data Structures/ES6/step027.js
+++ JavaScript Algorithms and Data Structures/ES6/step027.js
@@ -1,10 +1,10 @@
 const makeServerRequest = new Promise((resolve, reject) => {
-  // responseFromServer represents a response from a server
-  let responseFromServer;
-    
-  if(responseFromServer) {
-    // Change this line
-  } else {  
-    // Change this line
-  }
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
 });
