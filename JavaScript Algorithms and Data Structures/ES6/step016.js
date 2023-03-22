d92a5e66139ffb15e6e9cadedb60614f3a96d353
Preetham_dheeraj <32894920+preethamdheeraj@users.noreply.github.com>
2023-03-22 08:10:00 +0530

    Add files via upload

---
+++ JavaScript Algorithms and Data Structures/ES6/step016.js
@@ -0,0 +1,4 @@
+
+  // use object property shorthand to return an object with name, age, and gender properties
+  const createPerson = (name, age, gender) => ({name, age, gender});
+  // Only change code above this line
---
+++ JavaScript Algorithms and Data Structures/ES6/step018.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+class Vegetable {
+    constructor(name) {
+        this.name = name;
+    }
+}
+// Only change code above this line
+// upper camel case should be used while creating ES6 function names
+const carrot = new Vegetable("carrot");
+console.log(carrot.name); // Should display 'carrot'
