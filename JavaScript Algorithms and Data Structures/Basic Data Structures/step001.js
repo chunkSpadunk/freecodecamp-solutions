7d2f8ac7db6f2cf324e4f4639815d54096dd64ea
Preetham_dheeraj <32894920+preethamdheeraj@users.noreply.github.com>
2023-03-31 13:21:40 +0530

    Add files via upload

---
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/step001.js
@@ -0,0 +1,1 @@
+let yourArray = [1, 2, 3, "this is the string", false]; // Change this line
---
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/step002.js
@@ -0,0 +1,8 @@
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[0] = "a";
+myArray[1] = "this should be changed to anything but the letter b";
+myArray[2] = "c";
+myArray[3] = "d";
+// Only change code above this line
+console.log(myArray);
