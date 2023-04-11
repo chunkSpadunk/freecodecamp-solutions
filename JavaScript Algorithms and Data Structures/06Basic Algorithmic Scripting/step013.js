4f81aedef0d7e398a7adca06647f9ee96b743bb7
preethamdheeraj <chpreetham45@gmail.com>
2023-04-11 23:31:14 +0530

    added final file for the day

--- JavaScript Algorithms and Data Structures/06Basic Algorithmic Scripting/step013.js
+++ JavaScript Algorithms and Data Structures/06Basic Algorithmic Scripting/step013.js
@@ -3,3 +3,5 @@ function bouncer(arr) {
 }
 
 bouncer([7, "ate", "", false, 9]);
+
+// for this to work we need to remove all the falsy values from the array and see that we have the rest stored and set as per the main set of commands to see to it that it will work as intended
