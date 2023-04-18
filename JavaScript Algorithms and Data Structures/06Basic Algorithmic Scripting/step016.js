a8f023106e08c38f8d3ee1506fa261e4108064f0
preethamdheeraj <chpreetham45@gmail.com>
2023-04-18 20:03:27 +0530

    making small changes in the comments

--- JavaScript Algorithms and Data Structures/06Basic Algorithmic Scripting/step016.js
+++ JavaScript Algorithms and Data Structures/06Basic Algorithmic Scripting/step016.js
@@ -3,6 +3,8 @@ function chunkArrayInGroups(arr, size) {
         chunkSize = size;
         chunk = arr.slice(i, i + chunkSize);
         // splitting the array into the smallest chunks based on 2nd input
+        // the main line will split in this section and we will use all this to maintain all that we need for this to work
+        // this will be used to follow all that we need for this to work and then use all for the main lines that we will be using for this to maintain all the main workflows
     }
     return arr;
 }
