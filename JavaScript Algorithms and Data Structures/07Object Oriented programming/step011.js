f46109909ae850c09ee9ee634f470421faa9432b
preethamdheeraj <chpreetham45@gmail.com>
2023-04-24 19:40:30 +0530

    adding some more completed files

--- JavaScript Algorithms and Data Structures/07Object Oriented programming/step011.js
+++ JavaScript Algorithms and Data Structures/07Object Oriented programming/step011.js
@@ -10,3 +10,11 @@ let ownProps = [];
 let prototypeProps = [];
 
 // Only change code below this line
+// add all of the properties of beagle to the array ownProps
+for (let property in beagle) {
+    if (beagle.hasOwnProperty(property)) ownProps.push(property);
+    else prototypeProps.push(property);
+}
+
+console.log(ownProps);
+console.log(prototypeProps);
