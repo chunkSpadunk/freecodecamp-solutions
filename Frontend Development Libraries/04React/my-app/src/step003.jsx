357ec286be40f6545d003d5074e6dbaa31177b61
preethamdheeraj <chpreetham45@gmail.com>
2023-06-28 11:54:25 +0530

    making changes to the files of the react modules

--- Frontend Development Libraries/04React/my-app/src/step003.jsx
+++
@@ -1,7 +0,0 @@
-const JSX = (
-    <div>
-    {/* adding a comment here without changing the elements data of h1 and p */}
-      <h1>This is a block of JSX</h1>
-      <p>Here's a subtitle</p>
-    </div>
-  );
--- Frontend Development Libraries/04React/my-app/src/step007.jsx
+++
@@ -1,9 +0,0 @@
-const MyComponent = function() {
-    // only change the code below this line
-    return (
-        <div className="customClass">
-            this will contain some random text to show the text to demonstrate
-        </div>
-    )
-    // only change the code above this line
-};
